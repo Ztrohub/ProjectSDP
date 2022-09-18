a5792e22bdb6e8b32e6d21399d14506f49331c3a
Juan Alexanov <ajajuan10@gmail.com>
2022-09-18 20:30:27 +0700

    sek belum sleesai

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\SiteController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('pages.login');
+});
+
+Route::get('/dashboard', function(){
     return view('pages.dashboard');
 });
+
+Route::post('login', function(Request $request){
+    $uname = $request ->UNAME;
+    $upass =$request ->UPASS;
+
+    if($uname == "admin" && $upass == "admin"){
+        return redirect('/dashboard');
+    }
+})->name('login');
