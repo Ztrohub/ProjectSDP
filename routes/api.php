6794fc3e663dc2bfb8793d65a55938a753a181de
Fernando Natanael <itsmenandoo@gmail.com>
2022-12-03 16:04:52 +0700

    CRUD Item

--- routes/api.php
+++ routes/api.php
@@ -34,85 +34,111 @@ Route::middleware('auth:sanctum')->group(function(){
         ], 200);
     });
 
-    // == QUERY TO GET ALL ITEMS ==
-    Route::get('/getallitems', function(){
-        return response()->json([
-            'success' => true,
-            'message' => "Item berhasil didapatkan semua!",
-            'data' => App\Models\Item::get()
-        ], 200);
-    });
+    Route::prefix('item')->group(function(){
+        // == QUERY TO GET ALL ITEMS ==
+        Route::get('/getall', function(){
+            return response()->json([
+                'success' => true,
+                'message' => "Item berhasil didapatkan semua!",
+                'data' => App\Models\Item::get()
+            ], 200);
+        });
 
-    // == QUERY TO INSERT NEW ITEMS ==
-    Route::get('/getallitems', function(Request $request){
-        $fields = $request->validate([
-            'item_name' => "required",
-            'item_description' => "required",
-            'item_price' => "required|numeric|min:1000|bail",
-            'item_stock' => "required|numeric|min:1|bail"
-        ], [
-            'item_name.required' => ':attribute tidak boleh kosong!',
-            'item_description.required' => ':attribute tidak boleh kosong!',
-            'item_price.required' => ':attribute tidak boleh kosong!',
-            'item_price.numeric' => ':attribute harus berupa angka!',
-            'item_price.min' => ':attribute minimal Rp 1.000!',
-            'item_stock.required' => ':attribute tidak boleh kosong!',
-            'item_stock.numeric' => ':attribute harus berupa angka!',
-            'item_stock.min' => ':attribute minimal 1!'
-        ], [
-            'item_name' => 'Nama barang',
-            'item_description' => 'Deskripsi barang',
-            'item_price' => 'Harga barang',
-            'item_stock' => 'Jumlah stok'
-        ]);
+        // == QUERY TO INSERT NEW ITEMS ==
+        Route::get('/insert', function(Request $request){
+            $fields = $request->validate([
+                'item_name' => "required",
+                'item_description' => "required",
+                'item_price' => "required|numeric|min:1000|bail",
+                'item_stock' => "required|numeric|min:1|bail"
+            ], [
+                'item_name.required' => ':attribute tidak boleh kosong!',
+                'item_description.required' => ':attribute tidak boleh kosong!',
+                'item_price.required' => ':attribute tidak boleh kosong!',
+                'item_price.numeric' => ':attribute harus berupa angka!',
+                'item_price.min' => ':attribute minimal Rp 1.000!',
+                'item_stock.required' => ':attribute tidak boleh kosong!',
+                'item_stock.numeric' => ':attribute harus berupa angka!',
+                'item_stock.min' => ':attribute minimal 1!'
+            ], [
+                'item_name' => 'Nama barang',
+                'item_description' => 'Deskripsi barang',
+                'item_price' => 'Harga barang',
+                'item_stock' => 'Jumlah stok'
+            ]);
 
-        $item = Item::create([
-            'item_name' => $fields['item_name'],
-            'item_description' => $fields['item_description'],
-            'item_price' => $fields['item_price'],
-            'item_stock' => $fields['item_stock']
-        ]);
+            $item = Item::create([
+                'item_name' => $fields['item_name'],
+                'item_description' => $fields['item_description'],
+                'item_price' => $fields['item_price'],
+                'item_stock' => $fields['item_stock']
+            ]);
 
-        return response()->json([
-            'success' => true,
-            'message' => "Barang " . $item['item_name'] . " berhasil ditambahkan!"
-        ], 200);
-    });
+            return response()->json([
+                'success' => true,
+                'message' => "Barang " . $item['item_name'] . " berhasil ditambahkan!"
+            ], 200);
+        });
 
-    // == QUERY TO UPDATE ITEMS ==
-    Route::get('/getallitems', function(Request $request){
-        $fields = $request->validate([
-            'item_name' => "required",
-            'item_description' => "required",
-            'item_price' => "required|numeric|min:1000|bail",
-            'item_stock' => "required|numeric|min:1|bail"
-        ], [
-            'item_name.required' => ':attribute tidak boleh kosong!',
-            'item_description.required' => ':attribute tidak boleh kosong!',
-            'item_price.required' => ':attribute tidak boleh kosong!',
-            'item_price.numeric' => ':attribute harus berupa angka!',
-            'item_price.min' => ':attribute minimal Rp 1.000!',
-            'item_stock.required' => ':attribute tidak boleh kosong!',
-            'item_stock.numeric' => ':attribute harus berupa angka!',
-            'item_stock.min' => ':attribute minimal 1!'
-        ], [
-            'item_name' => 'Nama barang',
-            'item_description' => 'Deskripsi barang',
-            'item_price' => 'Harga barang',
-            'item_stock' => 'Jumlah stok'
-        ]);
+        // == QUERY TO UPDATE ITEMS ==
+        Route::get('/update', function(Request $request){
+            $fields = $request->validate([
+                'item_name' => "required",
+                'item_description' => "required",
+                'item_price' => "required|numeric|min:1000|bail",
+                'item_stock' => "required|numeric|min:1|bail"
+            ], [
+                'item_name.required' => ':attribute tidak boleh kosong!',
+                'item_description.required' => ':attribute tidak boleh kosong!',
+                'item_price.required' => ':attribute tidak boleh kosong!',
+                'item_price.numeric' => ':attribute harus berupa angka!',
+                'item_price.min' => ':attribute minimal Rp 1.000!',
+                'item_stock.required' => ':attribute tidak boleh kosong!',
+                'item_stock.numeric' => ':attribute harus berupa angka!',
+                'item_stock.min' => ':attribute minimal 1!'
+            ], [
+                'item_name' => 'Nama barang',
+                'item_description' => 'Deskripsi barang',
+                'item_price' => 'Harga barang',
+                'item_stock' => 'Jumlah stok'
+            ]);
 
-        $item = Item::where("item_id", $request->item_id);
-        $item->item_name = $fields['item_name'];
-        $item->item_description = $fields['item_description'];
-        $item->item_price = $fields['item_price'];
-        $item->item_stock = $fields['item_stock'];
-        $item->save();
+            $item = Item::where("item_id", $request->item_id)->first();
+            $item->item_name = $fields['item_name'];
+            $item->item_description = $fields['item_description'];
+            $item->item_price = $fields['item_price'];
+            $item->item_stock = $fields['item_stock'];
+            $item->save();
 
-        return response()->json([
-            'success' => true,
-            'message' => "Barang " . $item['item_name'] . " berhasil diupdate!"
-        ], 200);
+            return response()->json([
+                'success' => true,
+                'message' => "Barang " . $item['item_name'] . " berhasil diupdate!"
+            ], 200);
+        });
+
+        // == QUERY TO DELETE ITEMS ==
+        Route::get('/delete', function(Request $request){
+            $item = Item::where("item_id", $request->item_id)->first();
+            $item->deleted_at = Carbon\Carbon::now();
+            $item->save();
+
+            return response()->json([
+                'success' => true,
+                'message' => "Barang " . $item['item_name'] . " berhasil dihapus!"
+            ], 200);
+        });
+
+        // == QUERY TO RESTORE ITEMS ==
+        Route::get('/restore', function(Request $request){
+            $item = Item::where("item_id", $request->item_id)->first();
+            $item->deleted_at = null;
+            $item->save();
+
+            return response()->json([
+                'success' => true,
+                'message' => "Barang " . $item['item_name'] . " berhasil direstore!"
+            ], 200);
+        });
     });
 });
 
